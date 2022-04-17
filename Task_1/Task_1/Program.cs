3bf789dcd1ef2ae2f5b091207aae2183dbc8e14c
Feynyney <feynyney@gmail.com>
2022-04-17 18:26:43 +0300

    Task is done

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Task_1
+{
+
+
+
+    internal class Program
+    {
+
+        public static int CountWords(string Text)
+        {
+            int count_words = 1;
+
+            for (int i = 0; i < Text.Length; i++)
+            {
+                if (Text[i].ToString() == " " || Text[i] == '\n' || Text[i] == '\0')
+                {
+                    count_words++;
+                }
+            }
+            return count_words;
+        }
+
+        public static string[] StringDivision(string Text)
+        {
+
+            int count_words = CountWords(Text);
+
+            string[] arrStr = new string[count_words];
+
+            for (int i = 0; i < arrStr.Length; i++)
+            {
+                for (int j = 0; j < Text.Length; j++)
+                {
+                    if (Text[j].ToString() != " ")
+                    { 
+                        arrStr[i] = arrStr[i] + Text[j].ToString();
+                    }
+
+                    else if (Text[j].ToString() == " ")
+                    {
+                        i++;
+                    }
+
+                    else if (Text[j] == '\0')
+                    {
+                        break;
+                    }
+                }
+                break;
+            }
+            return arrStr;
+        }
+
+        public static void PrintNewLine(string[] arrStr)
+        {
+            for(int i = 0; i < arrStr.Length; i++)
+            {
+                Console.WriteLine(arrStr[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string Text = Console.ReadLine();
+
+            StringDivision(Text);
+
+            string[] arrStr = StringDivision(Text);
+            
+            PrintNewLine(arrStr);
+
+        }
+    }
+}
---
+++ Task_2/Task_2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Task_1
+{
+
+
+
+    internal class Program
+    {
+
+        public static int CountWords(string Text)
+        {
+            int count_words = 1;
+
+            for (int i = 0; i < Text.Length; i++)
+            {
+                if (Text[i].ToString() == " " || Text[i] == '\n' || Text[i] == '\0')
+                {
+                    count_words++;
+                }
+            }
+            return count_words;
+        }
+
+        public static string[] StringDivision(string Text)
+        {
+
+            int count_words = CountWords(Text);
+
+            string[] arrStr = new string[count_words];
+
+            for (int i = 0; i < arrStr.Length; i++)
+            {
+                for (int j = 0; j < Text.Length; j++)
+                {
+                    if (Text[j].ToString() != " ")
+                    {
+                        arrStr[i] = arrStr[i] + Text[j].ToString();
+                    }
+
+                    else if (Text[j].ToString() == " ")
+                    {
+                        i++;
+                    }
+
+                    else if (Text[j] == '\0')
+                    {
+                        break;
+                    }
+                }
+                break;
+            }
+            return arrStr;
+        }
+
+        public static void ReversWords(string Text)
+        {
+            StringDivision(Text);
+
+            string[] arrToRevers = StringDivision(Text);
+
+            Console.Write("Reversed string is: ");
+
+            for(int i = arrToRevers.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arrToRevers[i] + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string Text = Console.ReadLine();
+
+            ReversWords(Text);
+
+        }
+    }
+}
